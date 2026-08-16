6b138c1556dcaef0693915c64dd31fe7c17b814b
VIJAY KUMAR SUBRAMANIAN <vijay.samco@gmail.com>
2026-08-16 10:43:44 +0530

    Create __init__.py

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,14 @@
+"""
+FlowerVision AI
+
+Models Package
+
+This package is reserved for application models.
+
+Future additions may include:
+
+- SQLAlchemy database models
+- AI model wrappers
+- Domain models
+- Model metadata
+"""
